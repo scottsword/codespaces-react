77e9491584827e877ab26cfa831cd0da5e6652f1
Scott Sword <scott.sword@simplot.com>
2023-11-14 13:59:24 -0700

    Adds API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1 className="header">Web Exercise</h1>
         <p className="para">Task: Wire in a button that fetches data from this REST API and load it into a list.</p>
+        <p className="para">Endpoint: https://jsonplaceholder.typicode.com/users</p>
       </header>
     </div>
   );
